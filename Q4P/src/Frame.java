6e738eeeca85c9033cc17a7a9b63aa4dbf550552
1939622 <1939622@01-19810.pusd.dom>
2022-03-29 13:42:51 -0700

    3/29/22 bg, music, frame

--- Q4P/src/Frame.java
+++ Q4P/src/Frame.java
@@ -1,9 +1,98 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
-public class Frame {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	
+	//CREATE THE OBJECT (STEP 1)
+	
 
+	
+
+
+public static void main(String[] arg) {
+	Frame f = new Frame();
+}
+
+public Frame() {
+	JFrame f = new JFrame("frame");
+	f.setSize(new Dimension(720, 480));
+	f.setBackground(Color.blue);
+	f.add(this);
+	f.setResizable(false);
+	f.setLayout(new GridLayout(1,2));
+	f.addMouseListener(this);
+	f.addKeyListener(this);
+	Timer t = new Timer(16, this);
+	t.start();
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	f.setVisible(true);
 	}
 
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+	
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		 
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+	
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+			System.out.println(arg0.getKeyCode());
+	
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+	
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
