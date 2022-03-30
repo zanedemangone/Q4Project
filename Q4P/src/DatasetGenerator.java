56c2947f448f883aacb3e288aa735ac9a0d5110d
1960820 <1960820@01-19950.pusd.dom>
2022-03-30 13:54:41 -0700

    Failed miserably at JSON

--- Q4P/src/DatasetGenerator.java
+++ Q4P/src/DatasetGenerator.java
@@ -1,15 +1,31 @@
+import java.io.*;
+import java.util.*;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
 public class DatasetGenerator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] fN = {"John", "Joe", "Jason", "Jeremy", "Jane", "Joanne"};
-		String[] lN = {"A.", "B."};
+		JSONParser parser = new JSONParser();
 		
-		for(int i = 0; i < fN.length; i++) {
-			for(int j = 0; j < lN.length; j++) {
-				System.out.println(fN[i]+ " " + lN[j]);
+		try {
+			Object obj = parser.parse(new FileReader("data.json"));
+			JSONObject jsonObject = (JSONObject)obj;
+			String[] names = (String[])jsonObject.get("firstNames");
+			for(int i = 0 ; i < names.length; i++) {
+				System.out.print(names[i]+" ");
 			}
+			System.out.println();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
